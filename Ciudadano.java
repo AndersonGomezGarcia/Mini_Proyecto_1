e7e2911dc140b5cc44a2ba232dc112b0141513c2
JesusArenas4 <jesus.arenas@correounivalle.edu.co>
2023-10-11 16:08:23 -0500

    cambios

--- Ciudadano.java
+++ Ciudadano.java
@@ -7,14 +7,25 @@ public class Ciudadano {
     String cedula;
     String ciudad_origen;
     
-    Scanner scanner = new Scanner(System.in); 
+    
 
-    public Ciudadano(String nombre, String cedula, String ciudad_origen){
+    public Ciudadano() {
+}
+
+public Ciudadano(String nombre, String cedula, String ciudad_origen){
         this.nombre = nombre;
         this.cedula = cedula;
         this.ciudad_origen = ciudad_origen;
  
 } 
+
+
+
+
+@Override
+public String toString() {
+     return "Ciudadano [nombre=" + nombre + ", cedula=" + cedula + ", ciudad_origen=" + ciudad_origen + "]";
+}
 
 public String getNombre() {
      return nombre;
@@ -43,25 +54,35 @@ public void setCiudad_origen(String ciudad_origen) {
 
 //fghjkhgfd
 
-public static void pedir_ciudad(){
+public static String pedir_ciudad(){
      System.out.println("ahora seleccione la ciudad de origen: ");
+     String ciudad_digitada = "";
      for (Ciudad_origen ciudad : Ciudad_origen.values()){
           System.out.println("ciudad: " + ciudad);
           
-     }
+          Scanner elegir = new Scanner(System.in);
+          String ciudadDigitada = elegir.nextLine();
+          if (ciudadDigitada.equals(ciudad.toString())){
+              ciudad_digitada = ciudadDigitada;
+               break;
+               
+          }
 
+     }
+     
+     return ciudad_digitada;
 } 
 
 
-public static Ciudadano crear_Ciudadano(Scanner scanner){
+
+public static Ciudadano crear_Ciudadano(Scanner scanner, String ciudad_digitada){
      System.out.print("ingrese el nombre del ciudadano: ");   
      String nombre = scanner.nextLine();
      System.out.print("ingrese la cedula del ciudadano: ");   
      String cedula = scanner.nextLine();
-     System.out.print("ingrese la ciudad de origen del ciudadano: ");   
-     String ciudad_origen = scanner.nextLine();
+     Ciudadano.pedir_ciudad();
      
-     Ciudadano ciudadano = new Ciudadano(nombre, cedula, ciudad_origen);
+     Ciudadano ciudadano = new Ciudadano(nombre, cedula, ciudad_digitada);
      return ciudadano;
      }
 
