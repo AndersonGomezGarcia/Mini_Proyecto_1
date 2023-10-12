c827b4491808c5ce2a2d155dc45937ecaed9c26b
JesusArenas4 <jesus.arenas@correounivalle.edu.co>
2023-10-11 23:34:01 -0500

    Quitando cosas innecesarias de la clase ciudadano

--- Ciudadano.java
+++ Ciudadano.java
@@ -1,13 +1,8 @@
-import java.util.Scanner;
-
-
 public class Ciudadano {
 
     String nombre;
     String cedula;
     String ciudad_origen;
-    
-    
 
     public Ciudadano() {
 }
@@ -18,9 +13,6 @@ public Ciudadano(String nombre, String cedula, String ciudad_origen){
         this.ciudad_origen = ciudad_origen;
  
 } 
-
-
-
 
 @Override
 public String toString() {
@@ -52,41 +44,4 @@ public void setCiudad_origen(String ciudad_origen) {
 }
 
 
-//fghjkhgfd
-
-public static String pedir_ciudad(){
-     System.out.println("ahora seleccione la ciudad de origen: ");
-     String ciudad_digitada = "";
-     for (Ciudad_origen ciudad : Ciudad_origen.values()){
-          System.out.println("ciudad: " + ciudad);
-          
-          Scanner elegir = new Scanner(System.in);
-          String ciudadDigitada = elegir.nextLine();
-          if (ciudadDigitada.equals(ciudad.toString())){
-              ciudad_digitada = ciudadDigitada;
-               break;
-               
-          }
-
-     }
-     
-     return ciudad_digitada;
-} 
-
-
-
-public static Ciudadano crear_Ciudadano(Scanner scanner, String ciudad_digitada){
-     System.out.print("ingrese el nombre del ciudadano: ");   
-     String nombre = scanner.nextLine();
-     System.out.print("ingrese la cedula del ciudadano: ");   
-     String cedula = scanner.nextLine();
-     Ciudadano.pedir_ciudad();
-     
-     Ciudadano ciudadano = new Ciudadano(nombre, cedula, ciudad_digitada);
-     return ciudadano;
-     }
-
 }
-
-
-
