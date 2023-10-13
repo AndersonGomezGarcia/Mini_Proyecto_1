5fda2607ba7f5e1d53c2a00791e938be9a36f948
AndersonGomezGarcia <gomez.anderson@correounivalle.edu.co>
2023-10-12 22:35:02 -0500

    Funcionalidades completas

--- Ciudad_origen.java
+++
@@ -1,6 +0,0 @@
-public enum Ciudad_origen {
-    Cali, Palmira, Candelaria, Dagua, ElCerrito, Florida, Jamundi, LaCumbre, Pradera, Vijes, Yumbo,
-    tulua, buga;
- 
-}
-//fjafjkdhjlkfsakljflñsakjdfñlasjdfasjkdfñlshnldñ
